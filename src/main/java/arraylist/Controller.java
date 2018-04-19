5044c93b1cb5b1b9bafd9fa196f732a65e7527cc
Trevor Tabaka <trevortabaka@Trevors-MacBook-Pro.local>
2018-04-19 17:48:44 +0800

    Added unit testing to custom arraylist

--- src/main/java/arraylist/Controller.java
+++ src/main/java/arraylist/Controller.java
@@ -1,4 +1,27 @@
 package arraylist;
 
 public class Controller {
+
+    public static void main(String[] args) {
+
+        String string1 = "String1";
+        String string2 = "String2";
+        String string3 = "String3";
+        String string4 = "String4";
+        String string5 = "String5";
+
+        CustomArrayList customArrayList = new CustomArrayList();
+        System.out.println("new arraylist toString " + customArrayList.toString());
+        System.out.println("size " + customArrayList.size());
+
+        System.out.println("add 2 strings");
+        customArrayList.add(string1);
+        customArrayList.add(string2);
+
+        System.out.println("arraylist toString " + customArrayList.toString());
+
+
+
+
+    }
 }
--- src/test/java/arraylist/CustomArrayListTest.java
+++ src/test/java/arraylist/CustomArrayListTest.java
@@ -1,5 +1,58 @@
+package arraylist;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CustomArrayListTest {
+    CustomArrayList<String> arrayList = new CustomArrayList();
+    CustomArrayList<String> arrayList2 = new CustomArrayList(8);
 
+    @Before
+    public void loadArray(){
+        arrayList.add("test 0");
+        arrayList.add("test 1");
+        arrayList.add("test 2");
+
+        arrayList2.add("test 0");
+        arrayList2.add("test 1");
+        arrayList2.add("test 2");
+        arrayList2.add("test 3");
+
+    }
+
+    @Test
+    public void size() {
+
+        assertFalse(arrayList.size()==0);
+        assertEquals(arrayList.size(), 5);
+
+        assertEquals(arrayList2.size(), 8);
+
+    }
+
+    @Test
+    public void get() {
+        assertEquals("test 0", arrayList.get(0));
+
+    }
+
+
+    @Test
+    public void remove() {
+
+        assertTrue(arrayList2.get(2) == "test 2");
+        arrayList2.remove(2);
+        assertEquals(arrayList2.get(2), "test 3");
+
+    }
+    @Test
+    public void resize(){
+
+        assertEquals(arrayList.size(), 5);
+        arrayList.add("resize");
+        assertEquals(arrayList.size(),10);
+
+    }
 }
